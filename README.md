9dc8b915e889839aec02fec536785c5ea9f99877
LauraArmitage <little_laura1510@hotmail.co.uk>
2022-03-27 21:16:46 +0100

    Remove gulp entirely due to security vulnerabilities

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 
 
 1. Install Node https://nodejs.org/en/
-1. npm install 
-1. npm install --global gulp-cli
-1. gulp build
+1. npm i
+1. npm build
+1. npm upload
+
+### To Do
+
+* figure out how to build html without gulp
+* Add sass to build command
--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,8 @@
 var 
 	gulp = require('gulp'),
 	fileinclude = require('gulp-file-include'),
-	sass = require('gulp-sass')(require('sass')),
-  watch = require('gulp-watch');
+	sass = require('gulp-sass')(require('sass'));
  
-gulp.task('js', function() {
-	return gulp.src(['src/js/main.js'])
-		// todo minify
-		.pipe(gulp.dest('./build/'));
-});
-
 gulp.task('css', function() {
   return gulp.src(['src/scss/main.scss'])
     .pipe(sass().on('error', sass.logError))
@@ -27,23 +20,4 @@ gulp.task('html', function() {
 		.pipe(gulp.dest('./build/'));
 });
 
-gulp.task('img', function() {
-	return gulp.src(['img/*'])
-		.pipe(gulp.dest('./build/images/'));
-});
-
 gulp.task('build', gulp.series(['css', 'js', 'html', 'img']));
-
-gulp.task('watch-css', function () {
-  return gulp.watch('src/**/*.scss', sassbuild);
-});
-
-gulp.task('watch-js', function () {
-  return gulp.watch('src/**/*.js', jsbuild);
-});
-
-gulp.task('watch-html', function () {
-  return gulp.watch('src/**/*.html', htmlbuild);
-});
-
-gulp.task('watch', gulp.parallel(['watch-css', 'watch-js', 'watch-html']));
